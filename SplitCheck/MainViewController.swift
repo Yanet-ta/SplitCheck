9e78f1b6c2fea0096a5156a4e7d8b272a00da1c2
Yana Ivanova <suraeva.yana@gmail.com>
2017-01-18 02:22:36 +0300

    Add AVFoundation. Camera starts immediately when open the app.

--- SplitCheck/MainViewController.swift
+++ SplitCheck/MainViewController.swift
@@ -7,94 +7,62 @@
 //
 
 import UIKit
+import AVFoundation
 import MobileCoreServices
 
 class MainViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-   
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var doneButton: UIBarButtonItem!
+    var captureSession = AVCaptureSession()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    var captureDevice: AVCaptureDevice?
+
+  
+    @IBOutlet var previewView: UIView!
     
-    var newMedia: Bool?
-    var selectedImage: UIImage? {
-        didSet {
-            imageView.image = selectedImage
-        }
+    @IBOutlet weak var doneButton: UIBarButtonItem!
+    @IBAction func didTakePhoto(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Take photo"
-        self.doneButton.isEnabled = false
+        //self.doneButton.isEnabled = false
+        self.initCamera()
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "SYNMainToPreparationShowSegue" {
-            if let vc = segue.destination as? PreparationViewController {
-                if let image = selectedImage {
-                    vc.chosenImage = image
-                }
-            }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.previewLayer.frame = self.previewView.bounds;
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        captureSession.stopRunning()
+    }
+    
+    func initCamera() {
+        captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        do {
+            let inputCaptureDevice = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(inputCaptureDevice)
         }
-    }
-    
-    @IBAction func useCamera(_ sender: Any) {
-       self.startCamera()
-    }
-   
-    @IBAction func useCameraRoll(_ sender: Any) {
-        if UIImagePickerController.isSourceTypeAvailable(
-            UIImagePickerControllerSourceType.savedPhotosAlbum) {
-            let imagePicker = UIImagePickerController()
+        catch {
             
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-            imagePicker.mediaTypes = [kUTTypeImage as String]
-            imagePicker.allowsEditing = false
-            self.present(imagePicker, animated: true,
-                         completion: nil)
-            newMedia = false
         }
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        previewView.layer.addSublayer(previewLayer)
+        previewLayer.frame = previewView.bounds
     }
 
-    func startCamera() {
-        if UIImagePickerController.isCameraDeviceAvailable( UIImagePickerControllerCameraDevice.front) {
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
-            imagePicker.mediaTypes = [kUTTypeImage as String]
-            imagePicker.allowsEditing = false
-            
-            self.present(imagePicker, animated: true, completion: nil)
-            newMedia = true
-        }
-    }
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let mediaType = info[UIImagePickerControllerMediaType] as! NSString
-        self.dismiss(animated: true, completion: nil)
-        
-        if mediaType.isEqual(to: kUTTypeImage as String) {
-            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-            selectedImage = image
-            if (newMedia == true) {
-                UIImageWriteToSavedPhotosAlbum(image, self, #selector(MainViewController.image(image:didFinishSavingWithError:contextInfo:)), nil)
-                self.doneButton.isEnabled = true
-            }
-        }
-    }
-    
-    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
-        if error != nil {
-            let alert = UIAlertController(title: "Save Failed", message: "Failed to save image",preferredStyle: UIAlertControllerStyle.alert)
-            let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-            alert.addAction(cancelAction)
-            self.present(alert, animated: true, completion: nil)
-        }
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-    }
 }
 
